f219c3f96830c8eed9d413b8f23db3efaa10f550
Karson Kalt <kars.kalt@gmail.com>
2022-02-15 14:58:08 -0500

    Add greeting

--- section_5/interfaces/interface.ts
+++ section_5/interfaces/interface.ts
@@ -1,20 +1,22 @@
 interface Person {
-    name: string;
-    hungry: boolean;
+  name: string;
+  hungry: boolean;
 }
 
 // Interface for class, objects, and functions
-const karson: Person = {name: "karson", tired: false}
+const karson: Person = { name: "karson", tired: false };
 
 class Karson implements Person {
-    name: string;
-    hungry: boolean;
+  name: string;
+  hungry: boolean;
 }
 
 interface Greeting {
-    (name: string): string;
+  (name: string): string;
 }
 
 const myGreeting: Greeting = (name: string): string => {
-    return `Hello ${name}`
-}
+  return `Hello ${name}`;
+};
+
+class Greeting {}
